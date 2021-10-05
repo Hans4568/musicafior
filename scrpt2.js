0380d9e9067657045ce12906c3673318f6ddb94f
Hans <herlyaguilar1717@gmail.com>
2021-10-04 22:05:38 -0500

    Primer commit

---
+++ scrpt2.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "music1",
+    artist: "music1",
+    img: "music-1",
+    src: "music1"
+  },
+  {
+    name: "music2",
+    artist: "music2",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "music3",
+    artist: "music3",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "music4",
+    artist: "music4",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "music5",
+    artist: "music5",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "music6",
+    artist: "music6",
+    img: "music-6",
+    src: "music-6"
+  },
+]
